f16e431c11ef951b9b7f880a6f628fe8113979e2
FrankReque <fdrvega080206@gmail.com>
2026-04-27 14:34:36 -0500

    Se crea el componente para agendar citas

---
+++ front/src/app/components/agendar-cita/agendar-cita.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agendar-cita',
+  imports: [],
+  templateUrl: './agendar-cita.html',
+  styleUrl: './agendar-cita.css',
+})
+export class AgendarCita {
+
+}
