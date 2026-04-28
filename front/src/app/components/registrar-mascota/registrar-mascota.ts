afbb1aae3714d2dfb918000dea11501856dc6243
FrankReque <fdrvega080206@gmail.com>
2026-04-27 20:33:35 -0500

    Creación y diseño del modulo de registro de mascotas

---
+++ front/src/app/components/registrar-mascota/registrar-mascota.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registrar-mascota',
+  imports: [],
+  templateUrl: './registrar-mascota.html',
+  styleUrl: './registrar-mascota.css'
+})
+export class RegistrarMascota {
+
+}
