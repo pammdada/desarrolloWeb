0a19c0844c4003105e0fee2fbec3cec4728c6fc3
FrankReque <fdrvega080206@gmail.com>
2026-04-27 19:50:31 -0500

    Funcion Agendar cita hecha

--- front/src/app/components/veterinario/veterinario.ts
+++ front/src/app/components/veterinario/veterinario.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import { Citas } from '../../services/Citas/citas';
 
 @Component({
   selector: 'app-veterinario',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './veterinario.html',
   styleUrl: './veterinario.css',
 })
 export class Veterinario {
+  citasPendientes: any[] = [];
+
+  constructor(private citas: Citas) {}
+
+  ngOnInit(): void {
+    this.obtenerCitasPendientes();
+  }
+
+  obtenerCitasPendientes() {
+    this.citas.listarPendientes().subscribe({
+      next: (data) => {
+        this.citasPendientes = data;
+      },
+      error: (err) => {
+        console.error('Error al obtener citas pendientes', err);
+      }
+    });
+  }
+
+  gestionarCita(cita: any, nuevoEstado: string) {
+    this.citas.actualizarEstado(cita.id, nuevoEstado).subscribe({
+      next: () => {
+        alert(`La cita ha sido ${nuevoEstado.toLowerCase()} con éxito.`);
+        this.obtenerCitasPendientes();
+      },
+      error: (err) => {
+        console.error(`Error al ${nuevoEstado.toLowerCase()} la cita`, err);
+        alert(`Erro al actualizar la cita`);
+      }
+    });
+  }
 
 }
+
