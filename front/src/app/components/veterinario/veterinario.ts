5bf5d1f9d6db3c0e743878bc714eae88a323a699
Alonso Zegarra Velasquez <zegarrav24@gmail.com>
2026-04-30 13:37:39 -0500

    Agregando mensajes de aceptar y rechazar citas en la página del Veterinario

--- front/src/app/components/veterinario/veterinario.ts
+++ front/src/app/components/veterinario/veterinario.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Citas } from '../../services/Citas/citas';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-veterinario',
@@ -12,7 +13,7 @@ import { Citas } from '../../services/Citas/citas';
 export class Veterinario {
   citasPendientes: any[] = [];
 
-  constructor(private citas: Citas) { }
+  constructor(private citas: Citas) {}
 
   ngOnInit(): void {
     this.obtenerCitasPendientes();
@@ -25,6 +26,11 @@ export class Veterinario {
       },
       error: (err) => {
         console.error('Error al obtener citas pendientes', err);
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'No se pudieron cargar las citas pendientes'
+        });
       }
     });
   }
@@ -37,17 +43,48 @@ export class Veterinario {
       console.error("No se encontró el ID de la cita. Atributos disponibles:", Object.keys(cita));
       return;
     }
-    this.citas.actualizarEstado(idReal, nuevoEstado).subscribe({
+
+    if (nuevoEstado === "RECHAZADA") {
+      Swal.fire({
+        title: "¿Estás seguro?",
+        text: "Esta acción rechazará la cita",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#d33",
+        cancelButtonColor: "rgb(39, 204, 75)",
+        confirmButtonText: "Sí, rechazar",
+        cancelButtonText: "Cancelar"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.actualizarEstadoCita(idReal, nuevoEstado);
+        }
+      });
+    } else {
+      this.actualizarEstadoCita(idReal, nuevoEstado);
+    }
+  }
+
+  actualizarEstadoCita(id: number, estado: string) {
+    this.citas.actualizarEstado(id, estado).subscribe({
       next: () => {
-        alert(`La cita ha sido ${nuevoEstado.toLowerCase()} con éxito.`);
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `Cita ${estado.toLowerCase()} correctamente`,
+          showConfirmButton: false,
+          timer: 1500
+        });
         this.obtenerCitasPendientes();
       },
       error: (err) => {
-        console.error(`Error al ${nuevoEstado.toLowerCase()} la cita`, err);
-        alert(`Erro al actualizar la cita`);
+        console.error(`Error al ${estado.toLowerCase()} la cita`, err);
+        Swal.fire({
+          icon: "error",
+          title: "Error",
+          text: "No se pudo actualizar la cita"
+        });
       }
     });
   }
-
 }
 
